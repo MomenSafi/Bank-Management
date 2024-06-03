f02029f5d4132b04c37f8151e31182e7ba48ca53
Momen safi <Safi.moumen90@gmail.com>
2024-06-03 22:11:13 +0300

    Continue

--- BankManagment/UI/obj/Debug/net6.0/UI.AssemblyInfo.cs
+++ BankManagment/UI/obj/Debug/net6.0/UI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+49100e7c23a222a2217f1d6f70cd4cc144f3615e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7957ae592a71b776887767cca168eb530ff18e7")]
 [assembly: System.Reflection.AssemblyProductAttribute("UI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
